55d9ca5b9b97649aac30dfe771bd2124e28c4b8e
Saurabh Garg <143214670+SaurabhGarg6174@users.noreply.github.com>
2024-04-09 02:07:26 +0530

    Time: 5 ms (31.24%) | Memory: 10.9 MB (50.13%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -2,26 +2,55 @@ class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
         queue<int> q;
-        stack<int> st;
         int n = students.size();
-        for(int i = 0; i < n; i++) {
-            st.push(sandwiches[n - i - 1]);
+        for(int i=0;i<n;i++){
             q.push(students[i]);
         }
-        int i = 0; 
-        while (!st.empty() && i < n) {
-            if (q.front() == st.top()) {
-                st.pop();
-                q.pop();
-                i=0;
-            } 
-            else {
-                q.push(q.front());
+        int i = 0; //traverse the sandwiches array
+        int count = 0;
+        while(q.size()>0 && count!=q.size()){
+            if(q.front()==sandwiches[i]){
+                count = 0;
                 q.pop();
                 i++;
-            } 
+            }
+            else{
+                q.push(q.front());
+                q.pop();
+                count++;
+            }
         }
+        return q.size();
 
-        return st.size();
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int countStudents(vector<int>& students, vector<int>& sandwiches) {
+//         queue<int> q;
+//         stack<int> st;
+//         int n = students.size();
+//         for(int i = 0; i < n; i++) {
+//             st.push(sandwiches[n - i - 1]);
+//             q.push(students[i]);
+//         }
+//         int i = 0; 
+//         while (!st.empty() && i < n) {
+//             if (q.front() == st.top()) {
+//                 st.pop();
+//                 q.pop();
+//                 i=0;
+//             } 
+//             else {
+//                 q.push(q.front());
+//                 q.pop();
+//                 i++;
+//             } 
+//         }
+
+//         return st.size();
+//     }
+// };
