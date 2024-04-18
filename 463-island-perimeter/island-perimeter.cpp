14b18e6526be2fd77eb9928a9dc626aef4e17c71
Saurabh Garg <143214670+SaurabhGarg6174@users.noreply.github.com>
2024-04-18 13:15:39 +0530

    Time: 109 ms (8.61%) | Memory: 113.7 MB (5.12%) - LeetSync

---
+++ 463-island-perimeter/island-perimeter.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    int checkSurr(int i, int j, vector<vector<int>>& grid){
+        int surr = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<int> d_row = {0, -1, 0, 1};
+        vector<int> d_col = {-1, 0, 1, 0};
+        for(int k = 0; k < 4; k++){
+            int n_row = i + d_row[k];
+            int n_col = j + d_col[k];
+            if(n_row >= 0 && n_row < n && n_col >= 0 && n_col < m){
+                if(grid[n_row][n_col] == 1){
+                    surr++;
+                }
+            }
+        }
+        return surr;
+    }
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int ans = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        int surr;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 1){
+                    surr = checkSurr(i, j, grid);
+                    ans += 4-surr;
+                }
+            }
+        }
+        return ans;
+    }
+};
