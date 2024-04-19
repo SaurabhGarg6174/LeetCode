30bd1cb1950a1c03b2f55809b7b2266c91e3ac4e
Saurabh Garg <143214670+SaurabhGarg6174@users.noreply.github.com>
2024-04-19 23:16:19 +0530

    Time: 80 ms (9.01%) | Memory: 45.7 MB (5.19%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,21 +1,20 @@
 class Solution {    
 public:
-    void findLand(vector<vector<char>>& grid,vector<vector<char>>& vis, int i, int j){
+    void findLand(vector<vector<char>>& grid, int i, int j){
         int n = grid[0].size();
         int m = grid.size();
 
-        if(i<0 || i >= m || j<0 || j >= n || grid[i][j] == '0' || vis[i][j] == '1'){
+        if(i<0 || i >= m || j<0 || j >= n || grid[i][j] == '0'|| grid[i][j] == '2'){
             return;
         }
-
-        vis[i][j] = '1';
+        grid[i][j] = '2';
         vector<int> d_row = {0,-1,0,1};
         vector<int> d_col = {-1,0,1,0};
 
         for(int k = 0; k < 4; k++){
             int row = i + d_row[k];
             int col = j + d_col[k];
-            findLand(grid, vis, row, col);
+            findLand(grid, row, col);
         }
 
     }
@@ -23,11 +22,11 @@ public:
         int n = grid[0].size();
         int m = grid.size();
         int ans = 0;
-        vector<vector<char>> vis(m, vector<char>(n, '0'));
+
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(grid[i][j] == '1' && vis[i][j] != '1'){
-                    findLand(grid, vis, i, j);
+                if(grid[i][j] == '1'){
+                    findLand(grid, i, j);
                     ans++;
                 }
             }
